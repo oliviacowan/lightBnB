ed988ba2fe63b82cbcde35248c84983ac5ed561c
Olivia <olivia.cowan@hotmail.com>
2022-02-09 20:50:45 +0000

    create data base and tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (id, name, email, password) VALUES (1, 'Kevin Abstract', 'kaba@gmail.com', 'fhjJS7F');
+INSERT INTO users (id, name, email, password) VALUES (2, 'Frank Ocean', 'focean@gmail.com', 'jdsfk');
+INSERT INTO users (id, name, email, password) VALUES (3, 'Bad Bunny', 'bbunny@gmail.com', 'klasda');
+INSERT INTO users (id, name, email, password) VALUES (4, 'Matt Champ', 'mchamp@.com', 'nvnjsjsd');
+INSERT INTO users (id, name, email, password) VALUES (5, 'Karol G', 'kg@gmail.com', 'lk2dds7');
+
+
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id) VALUES (1, '2018-02-12', '2018-02-15', 4, 5);
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id) VALUES (2, '2018-02-12', '2018-02-15', 4, 5);
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id) VALUES (3, '2018-02-12', '2018-02-15', 4, 5);
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id) VALUES (4, '2018-02-12', '2018-02-15', 4, 5);
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id) VALUES (5, '2018-02-12', '2018-02-15', 4, 5);
