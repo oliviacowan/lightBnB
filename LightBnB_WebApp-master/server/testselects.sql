8a4322dfd51866d0cb086a7a046be7240941a385
Olivia <olivia.cowan@hotmail.com>
2022-02-10 21:10:44 +0000

    getAllReservations query

--- LightBnB_WebApp-master/server/testselects.sql
+++ LightBnB_WebApp-master/server/testselects.sql
@@ -1,7 +1,20 @@
 SELECT *
 FROM users
-WHERE email = 'g@com';
+LIMIT 10;
 
-INSERT INTO users (name, email, password) 
-VALUES ('Hey Grandma', 'gma@gmail.com', 'password')
-RETURNING *;
+-- INSERT INTO users (name, email, password) 
+-- VALUES ('Hey Grandma', 'gma@gmail.com', 'password')
+-- RETURNING *;
+
+SELECT reservations.guest_id, reservations.start_date, reservations.end_date, properties.thumbnail_photo_url, properties.number_of_bedrooms, properties.number_of_bathrooms, properties.parking_spaces
+FROM reservations
+WHERE guest_id = 12
+LIMIT 10;
+
+  -- return pool.query(
+  -- `SELECT reservations.*, properties
+  -- FROM reservations
+  -- WHERE guest_id = $1
+  -- JOIN properties ON properties.id = property_id
+  -- LIMIT $2;`, [guest_id, limit]
+  -- )
