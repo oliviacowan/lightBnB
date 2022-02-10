cb49c8ee6d30389decc2a2c9a7ec15f70bcddb96
Olivia <olivia.cowan@hotmail.com>
2022-02-10 22:37:39 +0000

    implement functions for search functionality

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -2,6 +2,7 @@ const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
 const { Pool } = require('pg');
+const { query } = require('express');
 
 
 const pool = new Pool({
@@ -117,13 +118,51 @@ exports.getAllReservations = getAllReservations;
 
 const getAllProperties = function(options, limit = 10) {
   
-  return pool.query(
-    `SELECT *
+  const queryParams = [];
+
+  let queryString = 
+    `SELECT properties.*, avg(property_reviews.rating) as average_rating
     FROM properties
-    LIMIT $1`, [limit]
-  )
-  .then((result) => result.rows)
-  .catch((err) => err.message);
+    JOIN property_reviews ON properties.id = property_id
+    `;
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(parseInt(options.minimum_price_per_night));
+    queryString += `AND cost_per_night >= $${queryParams.length}`;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(parseInt(options.maximum_price_per_night));
+    queryString += `AND cost_per_night <= $${queryParams.length}`;
+  }
+
+
+  if (options.owner_id) {
+    queryParams.push(parseInt(options.owner_id));
+    queryString += `
+    JOIN users ON users.id = owner_id
+    WHERE users.id = $${queryParams.length}`;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(parseInt(options.minimum_rating));
+    queryString += `AND rating >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += ` 
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length}
+  `;
+  
+  console.log(queryString, queryParams);
+  return pool.query(queryString, queryParams).then((res) => res.rows);
+  //.catch((err) => err.message);
 }
 
 
